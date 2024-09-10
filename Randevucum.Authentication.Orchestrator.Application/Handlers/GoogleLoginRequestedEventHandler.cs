08523d789e73bdc280afb9d0a66aec9d459c3256
Arda Burak Kontaş <aburakkontas@hotmail.com>
2024-09-11 00:23:50 +0300

    # Added explanation to google login requested event handler

--- Randevucum.Authentication.Orchestrator.Application/Handlers/GoogleLoginRequestedEventHandler.cs
+++ Randevucum.Authentication.Orchestrator.Application/Handlers/GoogleLoginRequestedEventHandler.cs
@@ -12,6 +12,9 @@ public class GoogleLoginRequestedEventHandler(IBus bus) : IConsumer<GoogleLoginR
         var requestEvent = new GoogleLoginReceivedEvent(context.Message.AuthenticationCode);
         var response = await _bus.Request<GoogleLoginReceivedEvent, GoogleLoginResultedEvent>(requestEvent);
 
+        // burası googleden veriyi alıp sonra Basic servisine göndericek ve oradan gelen veriyi dönecek dönüş tipide bu handlerin yeni bir event olacak AuthenticationResultedEvent olacak
+        // sonuçta yapmaya çalıştığımız şey Google'den veriyi alıp Basic ile aynı şekilde oluşturup yeni basic tokeni döndürmek...
+
         await context.RespondAsync(response.Message);
     }
 }
