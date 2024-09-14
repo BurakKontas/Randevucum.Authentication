a3ac998e16ce65f69685472b490ada9b2cc60aea
Arda Burak Kontaş <aburakkontas@hotmail.com>
2024-09-14 22:07:35 +0300

    # Added MediatR

---
+++ Randevucum.Authentication.Microservices.Basic.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Randevucum.Authentication.Microservices.Basic.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(ApplicationAssembly.Assembly));
+            return services;
+        }
+    }
+}
