6c99457f6c7d96f528d21acac5aa009c937a96a5
Arda Burak Kontaş <aburakkontas@hotmail.com>
2024-09-14 22:13:27 +0300

    Add DateTime parameter to UserLoggedInDomainEvent

--- Randevucum.Authentication.Microservices.Basic.Domain/DomainEvents/UserLoggedInDomainEvent.cs
+++ Randevucum.Authentication.Microservices.Basic.Domain/DomainEvents/UserLoggedInDomainEvent.cs
@@ -3,8 +3,8 @@ using Randevucum.Authentication.Microservices.Basic.Domain.ValueObjects;
 
 namespace Randevucum.Authentication.Microservices.Basic.Domain.DomainEvents;
 
-public record UserLoggedInDomainEvent(Guid Id, UserId UserId) : DomainEvent(Id)
+public record UserLoggedInDomainEvent(Guid Id, UserId UserId, DateTime DateTime) : DomainEvent(Id)
 {
-    public UserLoggedInDomainEvent(UserId UserId) : this(Guid.NewGuid(), UserId) 
+    public UserLoggedInDomainEvent(UserId UserId, DateTime DateTime) : this(Guid.NewGuid(), UserId, DateTime) 
     { }
 }
