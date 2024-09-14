4e7d8ff39d5b9fedce29a592309ea6215d3e1bac
Arda Burak Kontaş <aburakkontas@hotmail.com>
2024-09-14 22:03:34 +0300

    # Added DependencyInjection

---
+++ Randevucum.Authentication.Microservices.Basic.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Randevucum.Authentication.Microservices.Basic.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services;
+    }
+}
