b44f2083ef92ca7f71384d2d62325e970546db78
Arda Burak Kontaş <aburakkontas@hotmail.com>
2024-09-15 16:24:44 +0300

    Add CockroachDB options provider and DI integration
    Introduce `CockroachOptionsProvider` in the domain layer to manage CockroachDB read/write options. Update `DependencyInjection.cs` to integrate this provider with the DI system via `AddCockroachOptions`. Modify `Infrastructure.csproj` to reference the domain project.

---
+++ Randevucum.Authentication.Microservices.Basic.Domain/Providers/CockroachOptionsProvider.cs
@@ -0,0 +1,18 @@
+﻿using Randevucum.Authentication.Microservices.Basic.Domain.Options;
+
+namespace Randevucum.Authentication.Microservices.Basic.Domain.Providers;
+
+public static class CockroachOptionsProvider
+{
+    private static CockroachOptions? _readOptions;
+    private static CockroachOptions? _writeOptions;
+
+    public static void Configure(CockroachOptions readOptions, CockroachOptions writeOptions)
+    {
+        _readOptions = readOptions;
+        _writeOptions = writeOptions;
+    }
+
+    public static CockroachOptions? ReadOptions => _readOptions ?? throw new ArgumentNullException($"You need to configure CockroachOptionsProvider to use CockroachOptions.");
+    public static CockroachOptions? WriteOptions => _writeOptions ?? throw new ArgumentNullException($"You need to configure CockroachOptionsProvider to use CockroachOptions.");
+}
--- Randevucum.Authentication.Microservices.Basic.Infrastructure/DependencyInjection.cs
+++ Randevucum.Authentication.Microservices.Basic.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
+using System.Text.Json;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Randevucum.Authentication.Microservices.Basic.Domain.Options;
+using Randevucum.Authentication.Microservices.Basic.Domain.Providers;
 
 namespace Randevucum.Authentication.Microservices.Basic.Infrastructure;
 
@@ -8,6 +11,23 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddCockroachOptions(configuration);
         return services;
     }
+
+    public static void AddCockroachOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        var readOptionsValue = configuration.GetSection("CockroachDBRead").Value;
+        var writeOptionsValue = configuration.GetSection("CockroachDBWrite").Value;
+
+        if (readOptionsValue is null) throw new ArgumentNullException($"CockroachDBRead options are not found in the configuration.");
+        if (writeOptionsValue is null) throw new ArgumentNullException($"CockroachDBWrite options are not found in the configuration.");
+        
+        var readOptions = JsonSerializer.Deserialize<CockroachOptions>(readOptionsValue);
+        var writeOptions = JsonSerializer.Deserialize<CockroachOptions>(writeOptionsValue);
+
+        CockroachOptionsProvider.Configure(readOptions!, writeOptions!);
+    }
+
+
 }
