6dd88503d912185d7333105bee32f2b50fc1bff3
Arda Burak Kontaş <aburakkontas@hotmail.com>
2024-09-15 19:11:31 +0300

    Add services and project references for microservices
    Initialized WebApplication builder in Program.cs, added application and infrastructure services, and built and ran the application. Updated API.csproj to include project references to Application and Infrastructure projects.

--- Randevucum.Authentication.Microservices.Basic.API/Program.cs
+++ Randevucum.Authentication.Microservices.Basic.API/Program.cs
@@ -1,4 +1,10 @@
+using Randevucum.Authentication.Microservices.Basic.Application;
+using Randevucum.Authentication.Microservices.Basic.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplication(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
